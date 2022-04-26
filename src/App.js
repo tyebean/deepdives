df264125277780073c048ee41ce18a810f743789
tyebean <flower.webster@gmail.com>
2022-04-26 06:55:22 -0400

    setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Landing from './Landing/Landing';
+
+function App() {
+  return (
+    <div className="App">
+    
+      <Landing />
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Landing/Landing.jsx
@@ -0,0 +1,11 @@
+const Landing = () => {
+  const sayHello = "Hello"
+  return ( 
+    <>
+    <h1>This is Landing</h1>
+    {sayHello + " friend!"}
+    </>
+  );
+}
+
+export default Landing;
